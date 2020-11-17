3644e67efffd89b3269a3f80e9e9e233141f392b
issaZananiri <https://github.com/issaZananiri>
2020-11-17 02:54:40 +0200

    done

--- APIManager.js
+++ APIManager.js
@@ -4,4 +4,113 @@ class APIManager {
     constructor() {
         this.data = {}
     }
+    loadData()
+    {
+        this.data.Users = new fetchUser().loadData();
+        this.data.Quotes = new fetchquote().loadData();
+        this.data.Bacons = new fetchLorem().loadData();
+        this.data.Pokemons = new fetchPokemon().loadData();
+        return this.data;
+    }
+}
+class fetchUser extends APIManager {
+  
+    constructor()
+    {
+        super();
+        this.data.users = [];
+    }
+
+    loadData()
+    {
+        const dataa = [];
+    $.ajax({
+     method: "GET",
+    url: 'https://randomuser.me/api/?results=7',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      data.results.forEach(d => dataa.push({first:d.name.first, last:d.name.last, pic:d.picture.large, city:d.location.city, state:d.location.state}));
+           
+    },
+    error: function ( data ) {
+        console.log("error")
+          }
+  });
+  return dataa
+}}
+class fetchquote extends APIManager {
+  
+  constructor()
+  {
+      super();
+  }
+
+  loadData()
+  {
+      const dataa = [];
+    $.ajax({
+     method: "GET",
+    url: 'https://api.kanye.rest',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      dataa.push({quote:data.quote});
+    },
+    error: function ( data ) {
+        console.log("error")
+          }
+  });
+  return data
+}}
+class fetchPokemon extends APIManager {
+  
+  constructor()
+  {
+      super();
+  }
+
+  loadData()
+  {
+      const dataa = [];
+        $.ajax({
+     method: "GET",
+     
+    url: `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 801)}`,
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      dataa.push({image:data.sprites.front_default, text:data.name});
+    },
+    error: function ( data ) {
+        console.log("error")
+          }
+  });
+  return dataa
+}}
+class fetchLorem extends APIManager {
+  
+  constructor()
+  {
+      super();
+  }
+
+  loadData()
+  {
+      const dataa = [];{
+    $.ajax({
+     method: "GET",
+     
+    url: `https://baconipsum.com/api/?callback=?`,
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      dataa.push({bacon:data[0]});
+    },
+    error: function ( data ) {
+        console.log("error")
+          }
+  });
+  return dataa
+}}
 }
--- Renderer.js
+++ Renderer.js
@@ -1,4 +1,88 @@
+class Renderer
+{
+    constructor()
+    {
+        this.data = {};
+    }
 
-class Renderer {
-
+    renderData(data)
+    {
+        new RenderUsers().renderData(data.Users);
+        new RenderQuotes().renderData(data.Quotes);
+        new RenderBacons().renderData(data.Bacons);
+        new RenderPokemons().renderData(data.Pokemons);
+    }
 }
+
+class RenderUsers extends Renderer
+{
+    constructor()
+    {
+        super();
+       
+    }
+
+    renderData(data)
+    {
+        $(".user-container").empty();
+        const template = Handlebars.compile($('#usersData-temp').html());
+        $(".user-container").append(template(data[0]));
+        $(".friends-container").empty();
+        const template2 = Handlebars.compile($('#friendsData-temp').html());
+        $(".friends-container").append(template2(data.filter(friends => friends !== data[0])));
+    }
+}
+
+class RenderQuotes extends Renderer
+{
+    constructor()
+    {
+        super();
+    
+    }
+
+    renderData(data)
+    {
+        $(".quote-container").empty();
+        const template = Handlebars.compile($('#quotesData-temp').html());
+        $(".quote-container").append(template(data));
+    }
+}
+
+class RenderBacons extends Renderer
+{
+    constructor()
+    {
+        super();
+        
+    }
+
+    renderData(data)
+    {
+        $(".meat-container").empty();
+        const template = Handlebars.compile($('#baconsData-temp').html());
+        $(".meat-container").append(template(data));
+    }
+}
+
+class RenderPokemons extends Renderer
+{
+    constructor()
+    {
+        super();
+        
+    }
+
+    renderData(data)
+    {
+        $(".pokemon-container").empty();
+        const template = Handlebars.compile($('#pokemonsData-temp').html());
+        $(".pokemon-container").append(template(data));
+    }
+}
+
+Handlebars.registerHelper("capitalize", function(string)
+{
+    string = string || '';
+    return string.slice(0,1).toUpperCase() + string.slice(1);
+});
